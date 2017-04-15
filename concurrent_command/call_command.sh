51b802ae4f086c19b635c7dad927eb5288b774e3
unknown <lius30@CNENLIUS30L1C.corp.emc.com>
2017-04-15 14:47:47 +0800

    modify

--- concurrent_command/call_command.sh
+++ concurrent_command/call_command.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "uemcli -sslPolicy store -noheader -u local/admin -p Password123! -d $1 $2 > $3_$4.txt"
+echo "xxxxxxxxxxxxxx -s $1 $2 > $3_$4.txt"
 start_time=$(date +"%s")
 start_time_str="`date -d @$start_time +%Y-%m-%dT%H:%M:%S`"
-uemcli -sslPolicy store -noheader -u local/admin -p Password123! -d $1 $2 > $3_$4.txt
+xxxxxxxxxxxxxx -s $1 $2 > $3_$4.txt
 ret=$?
 end_time=$(date +"%s")
 end_time_str="`date -d @$end_time +%Y-%m-%dT%H:%M:%S`"
